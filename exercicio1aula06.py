7c0cbccaf9afbd08b32cde1bb175a5aa690c5b42
evertonleite1986 <65358459+evertonleite1986@users.noreply.github.com>
2021-05-11 20:46:58 -0300

    Add files via upload

---
+++ exercicio1aula06.py
@@ -0,0 +1,21 @@
+def soma(a1=0 , a2=0 , a3=0):
+    soma = a1 + a2 + a3
+    print(soma)
+
+
+soma(input('digite valor: '))
+
+
+
+
+def area(larg, comp):
+   a = larg * comp
+   print(f'A área de um terreno {larg} x {comp} é de {a}m²')
+
+l = float(input('Largura (m): '))
+c = float(input('Comprimento (m): '))
+area(l,c)
+
+
+
+
