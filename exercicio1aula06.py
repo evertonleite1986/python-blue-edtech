a31b78e9a23f9f789dcf0b9a082969ce6b63d189
evertonleite1986 <everton_leite25@hotmail.com>
2021-05-11 20:22:41 -0300

    novo 1

--- exercicio1aula06.py
+++ exercicio1aula06.py
@@ -8,3 +8,14 @@ soma(input('digite valor: '))
 
 
 
+def area(larg, comp):
+   a = larg * comp
+   print(f'A área de um terreno {larg} x {comp} é de {a}m²')
+
+l = float(input('Largura (m): '))
+c = float(input('Comprimento (m): '))
+area(l,c)
+
+
+
+
